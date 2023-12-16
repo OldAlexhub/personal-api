f3399eb410a7d4d55ad944b221fa315ec0c24f58
OldAlexhub <info@oldalexhub.com>
2023-12-15 23:26:42 -0500

    fixed an issue with fetching names

--- controllers/Login.js
+++ controllers/Login.js
@@ -5,10 +5,12 @@ import bcrypt from "bcrypt";
 const Login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await UserModel.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: `User doesn't exist` });
     }
+    // console.log("User:", user);
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(400).json({ message: `Invalid Credentials` });
@@ -17,10 +19,17 @@ const Login = async (req, res) => {
       expiresIn: process.env.JWT_EXPIRESIN,
     });
     const userId = user._id;
-
-    res
-      .status(200)
-      .json({ message: `Login Successfull`, userId, token: token });
+    const name = `${user.fname || ""} ${user.lname || ""}`.trim();
+    // if (!name) {
+    //   console.warn(`User's name is undefined or empty for email: ${email}`);
+    //   // Decide how to handle users without a name
+    // }
+    res.status(200).json({
+      message: `Login Successfull`,
+      userId,
+      token: token,
+      name,
+    });
   } catch (error) {
     res.status(500).json({ message: `Serve Error!` });
   }
